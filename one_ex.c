c54edc22736d0cb5d310a6c5dd1c52c5f7f1245a
vinothsha <74014926+vinothsha@users.noreply.github.com>
2021-04-24 07:18:20 +0530

    Add files via upload

---
+++ one_ex.c
@@ -0,0 +1,23 @@
+/*#include<stdio.h>
+int main(){
+  printf("%d",printf("%s","Hello World!"));
+}
+*/
+
+/*#include<stdio.h>
+int main(){printf("%10s","hello");}*/
+
+/*#include<stdio.h>
+int main()
+{
+  char c=255;
+  c=c+10;
+  printf("%d",c);
+}*/
+
+/*int main(){
+  unsigned i=1;
+  int j=-4;
+  printf("%u",i+j); if give "%d" it gives -3
+  return 0;
+}*/
