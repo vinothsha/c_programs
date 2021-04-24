ff3e9fe99d4d58fd6fb338bb97132eaaa317b352
vinothsha <74014926+vinothsha@users.noreply.github.com>
2021-04-24 10:31:56 +0530

    Add files via upload

---
+++ third_ex.c
@@ -0,0 +1,31 @@
+/*#include<stdio.h>
+int main()
+{
+      int var=0x43FF;
+      printf("%x",var);
+}//o/p is 43ff if give 0x43FF
+*/
+/*#include<stdio.h>
+int main()
+{
+      int var=0x43FF;
+      printf("%d",var);
+}//op is 17407*/
+
+/*#include<stdio.h>
+int main()
+{
+      int var=0x43ff;
+      printf("%x",var);
+}//o/p is 43ff if give 0x43ff ==0x43FF*/
+
+/*#include <stdio.h>
+static int i;
+static int i=27;
+static int i;
+int main()
+{
+        static int i;
+        printf("%d",i);
+        return 0;
+}*/
